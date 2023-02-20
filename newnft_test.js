b00c8e88c750e47d626d9fd97406a5b304b70208
ju4ncito <villarrealjm2@gmail.com>
2023-02-20 10:57:47 -0300

    Primer intento integracion, necesita solucion CORS

--- newnft_test.js
+++ newnft_test.js
@@ -1,5 +1,12 @@
-Feature('newnft');
+Feature("newnft");
 
-Scenario('test something',  ({ I }) => {
-
+Scenario("User adds NFT transaction", ({ I }) => {
+  I.amOnPage("https://afraid-tiger-production.up.railway.app/");
+  I.fillField("#nft", "CryptoPunk");
+  I.fillField("#price", "3");
+  I.selectOption("#status", "Sold");
+  I.click("Submit");
+  I.click("Submit");
+  I.waitForText("NFT added correctly", 20);
+  I.seeResponseCodeIs(200);
 });
